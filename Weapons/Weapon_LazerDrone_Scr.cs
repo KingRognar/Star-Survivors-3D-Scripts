377090df593fa7074d8de6bada65701cd47ef441
Nobody <Kingrognar@gmail.com>
2024-12-03 01:51:17 +0300

    Update 20 (3.12.2024)
    - continuation of work on Lazer Drones
    - now colliders fit vfx
    - added logic for shooting lazer
    - some polishing stuff
    - (2 TODOs done in Lazer Drones)

--- Weapons/Weapon_LazerDrone_Scr.cs
+++ Weapons/Weapon_LazerDrone_Scr.cs
@@ -1,4 +1,6 @@
+using System.Drawing;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class Weapon_LazerDrone_Scr : MonoBehaviour
 {
@@ -9,17 +11,23 @@ public class Weapon_LazerDrone_Scr : MonoBehaviour
     private float lastEnemySearchTime = 0f; private float searchDelay = 2f;
     private Transform targetTransform;
     [SerializeField] private LayerMask targetLayer;
-    private float trackingSpeed = 200f;
+    [SerializeField] private float lazerLifetime = 4f, shotDelay = 2f; private float nextShotToggle = 0f, shotStartTime; private bool isShooting = false;
+    [SerializeField] private float trackingSpeed = 100f;
+    [SerializeField] private AnimationCurve lazerLengthCurve, lazerScaleCurve;
+    private CapsuleCollider capsuleCollider;
+    private float size;
 
     //TODO: deal damage
-    //TODO: hook up vfx to attacks
-    //TODO: dmg colliders
     //TODO: separate left and right targets
     //TODO: make Weapon_LazerDrones_Scr
 
     private void Awake()
     {
         modelTransform = transform.GetChild(0);
+        capsuleCollider = GetComponentInChildren<CapsuleCollider>();
+        size = GetComponentInChildren<VisualEffect>().GetFloat("Size");
+        lazerLifetime = GetComponentInChildren<VisualEffect>().GetFloat("Lifetime");
+        capsuleCollider.gameObject.SetActive(false);
     }
 
     void Update()
@@ -36,7 +44,8 @@ public class Weapon_LazerDrone_Scr : MonoBehaviour
             SearchEnemy();
             lastEnemySearchTime += searchDelay;
         }
-        EnemyTracking();
+        TargetTracking();
+        ShootLazer();
     }
     private void MaintainDistance()
     {
@@ -44,20 +53,50 @@ public class Weapon_LazerDrone_Scr : MonoBehaviour
         newPosition += new Vector3(distanceToMaintain * (isRightDrone ? 1 : -1), 0, 0);
         transform.position = newPosition;
     }
-    private void RotateModel() //TODO: speedup when shooting
+    private void RotateModel()
     {
         modelTransform.Rotate(Vector3.forward, modelRotationSpeed * (isRightDrone ? -1 : 1) * Time.deltaTime, Space.Self);
     }
-    private void EnemyTracking()
+    private void TargetTracking()
     {
-        //TODO
         Vector3 targetPosition;
         if (targetTransform == null)
-            targetPosition = transform.position + new Vector3(0, 0, 5f);
+            targetPosition = new Vector3(0, 0, 5f);
         else
             targetPosition = targetTransform.position - transform.position;
 
         transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(targetPosition, Vector3.up), trackingSpeed * Time.deltaTime);
+    }
+    private void ShootLazer()
+    {
+        if (isShooting)
+            UpdateCollider();
+
+        if (nextShotToggle >= Time.time)
+            return;
+
+        if (isShooting)
+        {
+            nextShotToggle += shotDelay;
+            isShooting = false;
+            capsuleCollider.gameObject.SetActive(isShooting);
+            modelRotationSpeed = 25f;
+        }
+        else
+        {
+            shotStartTime = nextShotToggle;
+            nextShotToggle += lazerLifetime;
+            isShooting = true;
+            capsuleCollider.gameObject.SetActive(isShooting);
+            modelRotationSpeed = 75f;
+        }
+    }
+    private void UpdateCollider()
+    {
+        float t = (Time.time - shotStartTime) / lazerLifetime;
+        capsuleCollider.radius = size * lazerScaleCurve.Evaluate(t);
+        capsuleCollider.height = size * 17 * lazerLengthCurve.Evaluate(t);
+        capsuleCollider.center = new Vector3(0, 0, capsuleCollider.height / 2);
     }
     private void SearchEnemy()
     {
